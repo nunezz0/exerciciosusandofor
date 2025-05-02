ccb6a74e1779c00bf649d02e7c67d4fe52f96f49
Jenifer Nunes <160186691+nunezz0@users.noreply.github.com>
2025-05-02 19:17:50 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
-// 1 - Imprimir números de 1 a 10
+// 2 - Imprimir os números pares de 1 a 20
 
 #include <stdio.h>
 
 int main()
-
 {
     int i;
 
-    for( i = 1; i <= 10; i++)
-    {
-        printf("%d\n", i );
+    for( i = 1; i<=20; i++){
+        if ( i % 2 == 0) {
+            printf ("%d\n", i);
+        }
     }
+
     return 0;
 }
